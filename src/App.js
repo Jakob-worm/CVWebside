54d56872a4d5e041f4f849156591fd50c712bc8c
jakob <jkbwrm@gmial.com>
2023-08-21 18:08:37 +0200

    Adding header

--- src/App.js
+++ src/App.js
@@ -10,16 +10,28 @@ function MyButton () {
   )
 }
 
+function MyHeader () {
+    return (
+        <div>
+            <header className={'top-bar'}>
+                <p>
+                    hello
+                </p>
+            </header>
+        </div>)
+}
+
 function App () {
   return (
-    <div className="App">
-      <header className="CV-Website">
+    <div className='App'>
+        <MyHeader/>
+      <header className='CV-Website'>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className='App-link'
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
