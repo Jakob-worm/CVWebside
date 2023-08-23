4bb95b068cd7f718f8af868a66547815a5a9149e
Jakob Worm <au665910@uni.au.dk>
2023-08-23 12:22:21 +0200

    Staring on the about me css

--- src/ScrollContent/AboutMe.js
+++ src/ScrollContent/AboutMe.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import '../css/AboubtMe.css'
 
 // Import of image (adjust the path and filename as needed)
 import aboutMeImage from '../pictures/cocktail-svgrepo-com.svg'
