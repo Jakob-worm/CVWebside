c79aa8f55e5a42e91e51ab44f0eb925f5beff1ea
Jakob Worm <au665910@uni.au.dk>
2023-08-23 12:11:40 +0200

    Made the text bold and changed text color

--- src/ContactMeBar/ContactMeBar.js
+++ src/ContactMeBar/ContactMeBar.js
@@ -8,8 +8,13 @@ function ContactMeBar () {
   return (
         <div className="BottomBar">
             <div className="ContactMe-Text">{t('ContactMeText')}</div>
-            <a href="https://www.linkedin.com/in/jakob-worm-717720267/" target="_blank" rel="noopener noreferrer">
-                <div className="ContactMe-LinkedIn">LinkedIn</div>
+            <a
+                href="https://www.linkedin.com/in/jakob-worm-717720267/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="ContactMe-LinkedIn"
+            >
+                <div >LinkedIn</div>
             </a>
             <div className="ContactMe-Phone"> Tlf. +4552271200</div>
             <div className="ContactMe-Mail"> Email jkbwrm@gmail.com </div>
